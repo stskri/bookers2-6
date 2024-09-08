e0d0d045163a3d8c6d2ee431d8d8e7c007096cf8
stskri <177446189+stskri@users.noreply.github.com>
2024-09-08 11:27:44 +0000

    [finish]books_show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_comment = BookComment.new
+    @book_comments = BookComment.all
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  
+  
 end
